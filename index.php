702af34b178d6db7b1cb7ab867ff22af82dbc3d0
Nicolas Toporcov <ntoporcov@me.com>
2020-02-21 10:05:17 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- SEO Stuff-->
+    <title>SportPlay</title>
+    <meta property="description" content="SportsPlay is an upcoming iOS App for finding out where to watch sport events, and following your favorite teams.">
+    <meta property="og:title" content="SportsPlay | Follow your favorite teams">
+    <meta property="og:description" content="SportsPlay is an upcoming iOS App for finding out where to watch sport events, and following your favorite teams.">
+    <meta property="og:url" content="https://sportsplay.app">
+
+    <!-- HEAD Links go here -->
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+<div class="row">
+    <div class="col">
+        <img class="logo" alt="App Logo" src="/logo.png">
+        <div class="text">
+            <h1>Follow your favorite teams</h1>
+            <p>SportPlay is an iOS App currently in development. SportPlay will allow you to check where to watch your favorite teams, sports and leagues.</p>
+        </div>
+        <div class="text">
+            <h2>When will be SportPlay be available?</h2>
+            <p>My personal goal is to have it up and running before June. But we're still in the very early stages of design and development.</p>
+        </div>
+        <div class="text">
+            <h2>What sports will be available</h2>
+            <p>Sports planned for initial support are:</p>
+            <ul>
+                <li>Soccer</li>
+                <li>Motorsports</li>
+                <li>Baseball</li>
+                <li>Basketball</li>
+                <li>American Football</li>
+                <li>Ice Hockey</li>
+                <li>Rugby</li>
+                <li>Tennis</li>
+                <li>ESports</li>
+                <li>Volleyball</li>
+            </ul>
+        </div>
+        <div class="text">
+            <h2>Who are you?</h2>
+            <p>I'm Nic! You can find me in most platforms under the username <b>ntoporcov</b></p>
+        </div>
+    </div>
+    <div>
+        <img  alt="Image with Mockup of App on iPhone" class="hero" src="hero.jpg">
+    </div>
+</div>
+
+
+
+<!--In Body Scripts go Here-->
+</body>
+</html>
